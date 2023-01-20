1194bf577013c426a6fbd23be7c06ec0784f0961
Gautier Laurent <gautierlaurent47@gmail.com>
2023-01-20 09:22:33 +0100

    final validation

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,8 +12,10 @@ const modalbg = document.querySelector(".bground");
 const modalvalidation = document.querySelector(".validation");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const submitBtn = document.querySelector('.btn-submit')
-const formData = document.querySelectorAll(".formData");
+const formData = document.querySelector(".formData");
 const modalClose = document.querySelector(".close");
+const closeCroixValidation = document.querySelector(".close-validation")
+const modalBtnFermerValidation = document.querySelector(".btn-fermer-validation");
 
 // DOML Elements Errors
 const firstnameError = document.querySelector(".firstname");
@@ -37,14 +39,16 @@ const conditionGenerale = document.getElementById("checkbox1");
 
 
 // launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalBtn.forEach(btn => btn.addEventListener("click",launchModal));
 modalClose.addEventListener("click", closeModal);
+closeCroixValidation.addEventListener("click",closeInscription);
+modalBtnFermerValidation.addEventListener("click",closeInscription);
 firstNameInput.addEventListener("input", validePrenom);
 lastNameInput.addEventListener("input", valideNom);
 birthDateChoice.addEventListener("input", valideDate);
 quantityInput.addEventListener("input", valideQuantite);
 emailInput.addEventListener('input', valideEmail);
-conditionGenerale.addEventListener("input", checkCondition);
+conditionGenerale.addEventListener("input", cgu);
 submitBtn.addEventListener("click", validate);
 radios.forEach(radio => radio.addEventListener('input',valideVille));
 
@@ -58,21 +62,31 @@ function launchModal() {
 function closeModal() {
   modalbg.style.display = "none";
 }
+
+//fermer Inscription
+function closeInscription() {
+  modalvalidation.style.display="none";
+
+}
+
+
+// validation inscrip^tion
 function launchInscription() {
   closeModal();
   modalvalidation.style.display = "block";
 }
 
 
-
-
 // verifie que le champ Prénom est en alphabet avec minimum 2 caractère et max 20
 function validePrenom() {
   if (/^([a-zA-Z]{2,20}$)/.test(firstNameInput.value)) {
     firstnameError.setAttribute('data-error-visible', false);
+    return true;
+    
   }
   else {
     firstnameError.setAttribute('data-error-visible', true);
+    return false;
   }
 }
 
@@ -81,9 +95,11 @@ function validePrenom() {
 function valideNom() {
   if (/^([a-zA-Z]{2,20}$)/.test(lastNameInput.value)) {
     lastnameError.setAttribute('data-error-visible', false);
+    return true;
   }
   else {
     lastnameError.setAttribute('data-error-visible', true);
+    return false;
   }
 }
 
@@ -92,9 +108,11 @@ function valideNom() {
 function valideEmail() {
   if (/^[a-z0-9._-]+@{1}[a-z0-9.-_]{2,}[.]{1}[a-z]{2,5}$/.test(emailInput.value)) {
     mailError.setAttribute('data-error-visible', false);
+    return true;
   }
   else {
     mailError.setAttribute('data-error-visible', true);
+    return false;
   }
 }
 
@@ -105,9 +123,11 @@ function valideEmail() {
 function valideDate() {
   if (!birthDateChoice.value == "") {
     dateDeNaissanceError.setAttribute('data-error-visible', false);
+    return true;
   }
   else {
     dateDeNaissanceError.setAttribute('data-error-visible', true);
+    return false;
   }
 }
 // combien de tournoi 
@@ -115,9 +135,12 @@ function valideQuantite() {
   if (/^([1-9]{0,1}[0-9]$)/.test(quantityInput.value) &&
     quantityInput.value >= 0 && quantityInput.value < 100 && !quantity.value == "") {
     nbTournoiError.setAttribute('data-error-visible', false);
+    return true;
+
   }
   else {
     nbTournoiError.setAttribute('data-error-visible', true);
+    return false;
   }
 }
 
@@ -127,47 +150,43 @@ function valideVille(){
   for (var i = 0; i < radios.length; i++) {
     if (radios[i].checked) {
       checked = true;
-      console.log(radios[i].value);
       break;
-    
     }
   }
   if(checked) {
     radiosError.setAttribute('data-error-visible', false);
+    return true;
   } else {
     radiosError.setAttribute('data-error-visible', true);
+    return false;
   }
 }
 
 
 // vérifie  que le les conditions générales soient accéptées
-function checkCondition() {
+function cgu() {
   if (conditionGenerale.checked == true) {
     conditionGeneraleError.setAttribute('data-error-visible', false);
+    return true;
   }
   else {
     conditionGeneraleError.setAttribute('data-error-visible', true);
+    return false;
   }
 }
 
 
 // bouton submit (c'est parti)
 function validate(e) {
- 
+  e.preventDefault();
    
-    if (!validePrenom() && !valideNom() && !valideEmail() && !valideDate() && !valideQuantite() && !valideVille() && !checkCondition()) {
-      e.preventDefault(); 
-    
+    if (validePrenom() & valideNom() & valideEmail() & valideDate() & valideQuantite() & valideVille() & cgu()) {
+      launchInscription(); 
+ 
     }
-    else 
-   
-    launchInscription();
- 
-  }
- 
+}
 
   
-
 
 
 
